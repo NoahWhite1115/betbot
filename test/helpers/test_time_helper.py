210527cea7309c9c531d7766551bae0c4a320dab
Noah White <nawhite1115@gmail.com>
2022-08-12 21:43:26 -0700

    Undo breaking tests

--- test/helpers/test_time_helper.py
+++ test/helpers/test_time_helper.py
@@ -6,4 +6,4 @@ from datetime import datetime
 class TestApp(unittest.TestCase):
     def test_strToTime_happy_path(self):
         expected = datetime(2022, 12, 2, 0, 12, 32, 326000)
-        self.assertEqual(expected, src.helpers.time_helper.strToTime("2022-12-2 00:12:32"))
+        self.assertEqual(expected, src.helpers.time_helper.strToTime("2022-12-2 00:12:32.326"))
